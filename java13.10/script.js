8f4e9cef2a4a6c7d95c7200a18fb91bfa8c29770
alinedayane <contatoalinedayanee@gmail.com>
2025-10-16 21:17:02 -0300

    Add files via upload

--- java13.10/script.js
+++ java13.10/script.js
@@ -10,3 +10,15 @@ function verificarRefeicao() {
         resultado.textContent = "É jantar.";
     }
 }
+function teste(){
+ let pTexto = document.getElementById("paragrafo");
+ for(let i = 0 ; i < 5; i++){
+ pTexto.innerHTML = pTexto.innerHTML + "<br>" +" "+ i;
+ }
+}
+let novoParagrafo = document.getElementById ("paragrafo2")
+let lista = ["arroz", "salada", "picanha"]
+for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+    novoParagrafo.innerHTML = novoParagrafo.innerHTML + "<br>" + element;
+}
